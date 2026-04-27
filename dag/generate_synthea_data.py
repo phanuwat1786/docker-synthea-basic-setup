40b49d33181ff0140b2baa127eb7572230fae400
phanuwat1786 <panuwatlav@gmail.com>
2026-04-27 16:03:33 +0700

    update config and dag file

--- dag/generate_synthea_data.py
+++ dag/generate_synthea_data.py
@@ -1,48 +1,37 @@
 from airflow.sdk import DAG, task, Asset, Metadata
 import pendulum
 from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.standard.operators.bash import BashOperator
 import random 
 from docker.types import Mount
 
 with DAG(
     dag_id = 'generate_synthea_data',
     start_date = pendulum.parse('2026-04-24',tz = 'Asia/Bangkok'),
-    schedule = None
+    schedule = None,
+    max_active_runs = 1
 ) as dag:
 
+    state_list = [
+        "California","New York","Texas","Florida","Hawaii",
+        "Nevada","Alaska","Illinois","Massachusetts","Colorado"
+    ]
 
     @task()
-    def generate_command():
-        seed = 0
-        state_list = [
-            "Alabama", "Alaska", "Arizona", "Arkansas", "California", 
-            "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", 
-            "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", 
-            "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
-            "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
-            "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
-            "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", 
-            "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
-            "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
-            "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-            ]
-        commands_list = []
-        for state in state_list:
+    def generate_command(state):
+            seed = 0
+            population = random.randint(1_000,5_000)
 
-            population = random.randint(50_000,200_000)
-            commands_list.append(
-                f'java -jar /home/ubuntu/synthea-with-dependencies.jar "{state}" -p {population} -s {seed} -cs 1777013499694 -c /home/ubuntu/synthea/synthea.properties'
-            )
-        return commands_list
-    
-    t1 = generate_command()
+            return f'java -jar /home/ubuntu/synthea-with-dependencies.jar "{state}" -p {population} -c /home/ubuntu/synthea/synthea.properties'
 
-    t2 = DockerOperator.partial(
+    t_generate_command = generate_command.expand(state = state_list)
+
+    run_synthea_docker = DockerOperator.partial(
         task_id = 'run_synthea_docker',
         max_active_tis_per_dag = 1,
         image = 'synthea',
         mounts= [
-            Mount(source="{{ var.value.synthea_output_path }}", target='/home/ubuntu/output', type= 'bind'),
+            Mount(source="{{ var.value.synthea_output_path }}/batch{{ ti.map_index }}", target='/home/ubuntu/output', type= 'bind'),
             Mount(source="{{ var.value.synthea_config_path }}", target='/home/ubuntu/synthea', type= 'bind'),
         ],
         mount_tmp_dir=False,
@@ -51,7 +40,7 @@ with DAG(
         network_mode='pipeline-network',
         api_version="auto",
     ).expand(
-        command = t1
+        command = t_generate_command
     )
-    
-    t1 >> t2
+
+    t_generate_command >> run_synthea_docker
